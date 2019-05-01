19e24e990f6aaafe3d229e3f210603dc230d98ca
Michael-cuhk <michael.yunfei@gmail.com>
2019-05-01 22:56:40 +0200

    Intro Seaborn

--- LearnPython/LearnPython5II.py
+++ LearnPython/LearnPython5II.py
@@ -9,10 +9,7 @@
 # Seaborn is a Python data visualization library based on matplotlib.
 # It provides a high-level interface for drawing attractive and
 # informative statistical graphics.
-# plotnine is an implementation of a grammar of graphics in Python,
-# it is based on ggplot2. The grammar allows users to compose plots
-# by explicitly mapping data to the visual objects that make up the plot.
-# for plotnine, you need install: https://github.com/has2k1/plotnine
+
 
 import pandas as pd
 import matplotlib.pyplot as plt
